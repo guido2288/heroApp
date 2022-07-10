ce4f3f6c040cf89ce6f1eef250da5bdbc1a31b7b
Guido Garcia <guido2288@gmail.com>
2022-07-10 17:47:53 -0300

    Finish

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext";
 
@@ -6,13 +6,27 @@ export const LoginPage = () => {
 
   const { login } = useContext(AuthContext);
 
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value)
+
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 0) return;
+    setInputValue(inputValue);
+
+  }
+
   const navigate = useNavigate();
 
   const onLogin = () => {
 
     const lastPath = localStorage.getItem('lastPath') || '/';
 
-    login('Guido');
+    login(inputValue);
 
     navigate(lastPath, {
       replace: true
@@ -23,14 +37,26 @@ export const LoginPage = () => {
     <div className="container mt-5">
       <h1>Login</h1>
       <hr />
+      <form onSubmit={onSubmit}>
+        <input
+          className="form-control mb-2"
+          type="text"
+          placeholder="Enter your name"
+          value={inputValue}
+          onChange={onInputChange}
+        />
 
 
-      <button
-        className="btn btn-primary"
-        onClick={onLogin}
-      >
-        Login
-      </button>
+        <button
+          className="btn btn-primary"
+          onClick={onLogin}
+        >
+          Login
+        </button>
+
+
+      </form>
+
     </div>
   )
 }
