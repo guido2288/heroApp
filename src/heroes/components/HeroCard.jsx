1fa582df7bc8ddc833e9c9b1df96b443708aae6f
Guido Garcia <guido2288@gmail.com>
2022-07-10 17:52:27 -0300

    Fix Images

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters, }) => {
 
-  const heroImage = `/assets/heroes/${id}.jpg`;
+  const heroImage = `./assets/heroes/${id}.jpg`;
 
   const charactersByAlterEgo = (<p>{characters}</p>);
   return (
